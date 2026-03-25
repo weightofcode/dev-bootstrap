a84311272d738446f32ddc0c3265d3038c4dc8dd
weightofcode <czorile@gmail.com>
2026-03-25 22:36:12 +0200

    strings - basics

--- python/src/tutorial.py
+++ python/src/tutorial.py
@@ -78,3 +78,69 @@ import random
 print(random.randrange(-2, 14))
 """
 
+
+print('###############################################')
+
+# a = """
+# this is a multi-line string
+# really, it is a string
+# """
+# print(a)
+# 
+# b = '''
+# this is also a multi-line string
+# with single quotes
+# '''
+# print(b)
+# 
+# # loop strings (arrays of chars)
+# c = "hello"
+# print(c[2])
+# print(c)
+# for i in c:
+#     return(i)
+# 
+# # str length
+# print('length of c:', len(c))
+
+# check str
+x = "hello world"
+print('hi in x:', 'hi' in x)
+
+t = str(4+5+10)
+m = str(1)
+n = str(2)
+if m in t:
+    print('m=1 in t:', bool(m))
+if n not in t:
+    print('n=2 not in t:', bool(n))
+
+# --------- SLICE ----------------
+x = "greetings"
+i = (x[3:6])
+j = x[:len(x)]
+print("i:", i, "len(i):", len(i))
+print("j:", j, "len(j):", len(j))
+
+# --------- MODIFY ----------------
+
+name = '"Harald Konrad "'
+u = name.upper()
+print(u)
+l = name.lower()
+print(l)
+s = name.strip() # remove whitespace
+print(s)
+r = name.replace('a', 'u')
+print(r)
+
+# --------- FORMAT ----------------
+age = 45
+txt = f"age is {age}"
+print(txt)
+price = 59
+# placeholder can include MODIFIER or Python code
+txt1 = f"price is {price:.2f} $" 
+print(txt1)
+txt2 = f"price is twice expensive {59 * 2} " 
+print(txt2)
