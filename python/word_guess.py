f35416b2c6aefa779e107c0c4298d1b9c3139ee3
weightofcode <czorile@gmail.com>
2026-04-05 20:36:57 +0300

    word guess - more code

--- python/word_guess.py
+++ python/word_guess.py
@@ -22,6 +22,9 @@ import random
 #     check if user has lost
 #     end
 
+
+MAX_ALLOW_TRIES = 8
+
 def define_word_list():
     words_to_pick_from = [
             "apple", "orange", "banana", "kiwi", "pear", "plum", "pineapple", 
@@ -44,11 +47,38 @@ def greet_user():
     print("Hello", username)
     print("-------------------------------------------")
 
+
 if __name__ == "__main__":
     greet_user()
 
-    # ask for char form user
-    # is char in word -> give feedback
+    # ask for char from user
+    # is char in word?
     #   if yes -> ask user to guess the word
     #       print the success if word is a match with random word
     #   if not -> try again and decrease try count
+
+    char_list = []
+    word = get_word_from_list()
+    error_counter = 8
+    word_guess = ""
+    while error_counter < MAX_ALLOW_TRIES:
+        print("-------------------------------------------")
+        c = read_char_from_input()
+        if c in word:
+            print(f"The character [{c}] exits in the word!")
+            char_list.append(c)
+            print(f"Current guessed characters: {char_list}")
+            print("Want to guess the word?")
+            if word_guess:
+                gw = input("Guess the word: ", )
+                if word_guess == word:
+                    print(f"SUCCESS! The word was: {word}")
+                else:
+                    print("Try again?")
+                    # where do we stop? this seems almost correct
+                    # TODO: fix it
+        else:
+            print(f"The character [{c}] does not exist in the word. Try again!")
+            error_counter += 1
+
+
