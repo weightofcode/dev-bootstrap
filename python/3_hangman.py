597e82d61f26ba1bbc4ca6f3a1a2bf3a9064b742
weightofcode <czorile@gmail.com>
2026-05-02 14:24:13 +0300

    fix: calculate correctly the number of guessed chars

--- python/3_hangman.py
+++ python/3_hangman.py
@@ -1,3 +1,4 @@
+# The Hangman Game
 # - The program randomly selects a word from a list of secret words.
 # - The player has limited chances to guess the word.
 # - When a correct letter is guessed, it is revealed in its correct position.
@@ -33,26 +34,27 @@ def obfuscate_word(rand_word):  # >>>>
 def guess_random_word(rand_word, obfuscated_word):  # >>>>
     attempt = 0
     max_attempts = len(rand_word) + 2
-    chars_to_guess = len(rand_word)
-
-    # some subtraction operation here to calculate the value of this data
-    actual_remaining_not_guessed_chars = 0 
-
     print(f"DEBUG1: rand_word = {rand_word}")
+    # TODO: Handle duplicate guesses
+    # if a user entered again an already guessed character, inform the user and return to input() call
+    # otherwise we do the whole math again with those characters, which is wrong
     while attempt < max_attempts:
+        chars_to_guess = len(rand_word)
+        number_of_guessed_chars = 0 # move inside while loop to reset counter to 0, to avoid incorrect calculation of guessed chars
         inp = input("Enter a character: ")
         attempt += 1
         print(f"INFO: Attempt: {attempt}. Attempts left: {max_attempts - attempt}")
         for i, c in enumerate(rand_word):
             if c == inp:
                 obfuscated_word[i] = inp
-                chars_to_guess -= 1
-                print(f"DEBUG2: chars_to_guess = {chars_to_guess}")
-                # TODO: Success condition works incorrectly in case multiple chars 
-                # are guessed with the same input value
-                if chars_to_guess == 0:
-                    print("SUCCESS! You guessed the word!")
-                    return
+                number_of_guessed_chars += 1
+        chars_to_guess = chars_to_guess - number_of_guessed_chars
+        print(f"DEBUG2: chars_to_guess = {chars_to_guess}")
+        if chars_to_guess == 0:
+            print("SUCCESS! You guessed the word!")
+            return
+        else:
+            print("FAILURE! Better luck next time!") # TODO: is this OK here?
         # TODO: Display explicit failure message
         print(f"WORD TO GUESS: {obfuscated_word}")
         print("---------------------------------------------------------------------")
